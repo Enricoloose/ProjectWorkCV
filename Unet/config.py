36f527792a80f548f50b04d368fbd677454d0d91
Enricoloose <104847152+Enricoloose@users.noreply.github.com>
2026-06-04 15:44:18 +0200

    Add configuration file for dataset paths and labels

---
+++ Unet/config.py
@@ -0,0 +1,31 @@
+import os
+
+
+def checkPath(path):
+    if(os.path.exists(path)):
+        print(path)
+    else:
+        print(f'Percorso: {path} non trovato')
+
+CURRENT_DIR = os.getcwd()
+
+#INIZIALIZZAZIONE DIRECTORY PER I DATASET
+PARENT_DIR = os.path.dirname(CURRENT_DIR)
+DATASET_DIR = os.path.join(PARENT_DIR,'data')
+
+data_name = 'MVTec1'
+MVTEC_DIR = os.path.join(DATASET_DIR, data_name)
+#checkPath(MVTEC_DIR)
+dtd_name = 'dtd'
+DTD_DIR = os.path.join(DATASET_DIR,dtd_name,'images')
+#checkPath(DTD_DIR)
+
+#Categorie da codificare
+LABELS = ['leather', 'pill', 'tile', 'transistor', 'wood']
+
+
+#Object mi serve per capire se l'immagine contiene un oggetto,
+#e di conseguenza applicare il rumore di perlin
+L_OBJECT = ['pill','transistor']
+
+
